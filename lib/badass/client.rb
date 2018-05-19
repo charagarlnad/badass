05e2016ba833b46684eb3dcaf09abbc4e5bcf575
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-05-19 02:31:59 -0400

    remove some debugging stuff oops

--- lib/badass/client.rb
+++ lib/badass/client.rb
@@ -7,7 +7,6 @@ class BadASS::Client
         @baddragon_skus[toy['sku']] = toy['name']
       end
       loop do
-        start_time = Time.now
         @toys_finished = false
         page = 1
         @toys = []
@@ -21,7 +20,6 @@ class BadASS::Client
           break if page > newtoys['totalPages']
         end
         @toys_finished = true
-        puts (Time.now - start_time) * 1000
         sleep(refresh_time)
       end
     end
